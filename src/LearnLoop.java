a79d3f7aed97d75756ee5188171c3a07894b4176
elush <elush@10.0.0.153>
2023-11-17 19:26:27 -0800

    Initial Project Setup

---
+++ src/LearnLoop.java
@@ -0,0 +1,10 @@
+public class LearnLoop {
+
+	public static void main(String[] args) {
+		
+		TableOf4999 Objtable = new TableOf4999 ();
+		Objtable.Domultiply();
+	}
+
+}
+
---
+++ src/TableOf4999.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TableOf4999 {
+
+	public void Domultiply( )
+	{
+		int tablenumber = 4999;
+		for (int i=1; i<=10; i++)
+			{
+				int result= tablenumber*i;
+				System.out.println(tablenumber + "*" + i+ "=" + result);
+			}
+			
+	}
+	
+}
+
+
